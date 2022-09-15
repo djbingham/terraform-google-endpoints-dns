cb530f7999d92e3487f5921cacf0e0e8cbc5b809
David Bingham <git@davidjbingham.co.uk>
2022-09-15 13:51:24 +0100

    Add types to all variables

--- variables.tf
+++ variables.tf
@@ -16,19 +16,23 @@
 
 variable "project" {
   description = "Project to create the Cloud Endpoint service in. If not given, the default provider is used."
+  type        = string
   default     = ""
 }
 
 variable "name" {
   description = "Name of the cloud endpoints service. This will create a DNS record in the form of: NAME.endpoints.PROJECT.cloud.goog"
+  type        = string
 }
 
 variable "external_ip" {
   description = "External IP the endpoint will point to."
+  type        = string
 }
 
 variable "ensure_undelete" {
   description = "Run gcloud command before creating cloud endpoint to force undelete of service endpoint. If endpoint has recently been deleted, it cannot be re-created without first undeleting it."
+  type        = bool
   default     = true
 }
 
@@ -46,5 +50,6 @@ variable "gcloud_cache_path" {
 
 variable "use_tf_google_credentials_env_var" {
   description = "Use `GOOGLE_CREDENTIALS` environment variable to run `gcloud auth activate-service-account` with."
+  type        = bool
   default     = false
 }
